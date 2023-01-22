8fc02b41e2ccfc7ca5ef907664c284a8fa60dc3c
Nicholaus Clark <nicholaus.clark115@gmail.com>
2023-01-21 20:56:36 -0500

    CLI arguments

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,28 +1,50 @@
 import { bake, ExporterRegistry, include, State } from "@premake-core/core";
+import { command } from "cmd-ts";
+import { File } from "cmd-ts/batteries/fs";
+import { option } from "cmd-ts/dist/cjs/option";
+import { positional } from "cmd-ts/dist/cjs/positional";
+import { run } from "cmd-ts/dist/cjs/runner";
+import { string } from "cmd-ts/dist/cjs/types";
 
 // Load all the core modules
-require("@premake-core/vstudio")
-require("@premake-core/msc")
+require("@premake-core/vstudio");
+require("@premake-core/msc");
 
-// Execute the premake configuration script
-const filepath = './examples/premake6.js';
-const domState = include(filepath);
+const app = command({
+    name: 'premake',
+    args: {
+        exporterName: positional({
+            type: string,
+            displayName: 'Exporter Name'
+        }),
+        scriptPath: option({
+            type: File,
+            long: 'script-path',
+            short: 's'
+        })
+    },
+    handler: ({ exporterName, scriptPath }) => {
+        // Execute the premake configuration script
+        const filepath = scriptPath || './premake6.js';
+        include(filepath);
 
-// Validate the exporter chosen.  This must be done after loading the configuration script in case
-// the configuration script loads an exporter.
+        // Validate the exporter chosen.  This must be done after loading the configuration script in case
+        // the configuration script loads an exporter.
 
-const exporterName = 'vstudio'; // TODO: Load from CLI arguments
-const exporter = ExporterRegistry.get().fetch(exporterName);
-if (!exporter) {
-    throw new Error(`Could not find exporter with name ${exporterName}`);
-}
+        const exporter = ExporterRegistry.get().fetch(exporterName);
+        if (!exporter) {
+            throw new Error(`Could not find exporter with name ${exporterName}`);
+        }
 
+        // Bake the configuration state into a format the exporters can use
+        bake(State.get());
 
-// Bake the configuration state into a format the exporters can use
-bake(State.get());
-
-// Use the selected exporter to write the state to file(s)
-exporter.functor(domState, {
-    name: exporterName,
-    version: "2022"
+        // Use the selected exporter to write the state to file(s)
+        exporter.functor(State.get(), {
+            name: exporterName,
+            version: "2022"
+        });
+    },
 });
+
+run(app, process.argv.slice(2));
